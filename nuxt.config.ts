9b8278ed5f688758bdf3e7bdabd779720b187755
Yusuf <yusufonarandx@hotmail.com>
2025-05-04 16:57:50 +0300

    fix: i18n config updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,38 +1,42 @@
 import tailwindcss from "@tailwindcss/vite";
-import { defineNuxtConfig } from 'nuxt/config'
+import { defineNuxtConfig } from "nuxt/config";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
   vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
   },
-  modules: ['@nuxtjs/i18n', '@nuxtjs/color-mode'],
+  modules: ["@nuxtjs/i18n", "@nuxtjs/color-mode"],
   i18n: {
-    defaultLocale: 'en',
-    langDir: 'locales',
+    defaultLocale: "en",
+    langDir: "locales",
     lazy: true,
-    detectBrowserLanguage: false,
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: "i18n_locale",
+      fallbackLocale: "en",
+      alwaysRedirect: false,
+      redirectOn: "root", 
+    },
     locales: [
       {
-        code: 'en',
-        name: 'English',
-        file: 'en.json',
+        code: "en",
+        name: "English",
+        file: "en.json",
       },
       {
-        code: 'tr',
-        name: 'Türkçe',
-        file: 'tr.json',
+        code: "tr",
+        name: "Türkçe",
+        file: "tr.json",
       },
     ],
     strategy: "no_prefix",
   },
   colorMode: {
-    classSuffix: '',
-    preference: 'light',
-    fallback: 'light',
-  }
+    classSuffix: "",
+    preference: "light",
+    fallback: "light",
+  },
 });
