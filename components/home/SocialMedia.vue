d80332cc643998aa78e615e1ab5251a7d9fe8385
Yusuf <yusufonarandx@hotmail.com>
2025-05-08 00:34:57 +0300

    style: update component styles

--- components/home/SocialMedia.vue
+++ components/home/SocialMedia.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex items-center justify-center gap-6 sm:gap-10 my-7 z-10">
     <NuxtLink v-for="social in socials" :key="social.name" :href="social.link" target="_blank" rel="noopener noreferrer"
-      class="flex items-center justify-center cursor-pointer text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-white transition-colors duration-300"
+      class="flex items-center justify-center cursor-pointer text-gray-600 hover:text-gray-800 dark:text-gray-700 dark:hover:text-gray-900 transition-colors duration-300"
       :aria-label="`Go to ${social.name} profile`">
       <component :is="social.logo" class="social-item w-6 h-6" aria-hidden="true" :fontControlled="false" />
     </NuxtLink>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,40 @@
 <template>
-  <div class="min-h-screen flex flex-col items-center justify-center transition-colors duration-300
-              bg-white text-gray-800 dark:bg-gray-900 dark:text-gray-100 text-center">
+  <div class="min-h-screen flex flex-col items-center justify-center transition-colors duration-300 relative z-1">
+    <div class="m-5">
+      <NuxtImg loading="lazy" class="rounded-full h-28 w-28 object-cover" src="/assets/yusuf.png"
+        alt="Yusuf - Web Developer Portrait Photo" />
+    </div>
     <div>
-      <h1 class="text-4xl font-bold underline mb-4">
-        {{ t('greeting') }}
+      <h1 class="text-center align-top tracking-wider font-bold text-5xl text-header mb-5 acorn">
+        {{ t('index.greeting') }}
       </h1>
+    </div>
+    <div>
+      <h3 class="text-color-text font-medium text-center">
+        {{ t('index.about') }}
+      </h3>
+    </div>
+    <div>
+      <SocialMedia class="z-1" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import SocialMedia from '~/components/home/SocialMedia.vue'
+
 const { t } = useI18n()
 </script>
+
+<style scoped>
+@font-face {
+  font-family: "Acorn";
+  src: url("/assets/fonts/Acorn/Acorn-Bold.woff2") format("woff2");
+  font-weight: 700;
+  font-style: normal;
+}
+
+.acorn {
+  font-family: 'Acorn', sans-serif;
+}
+</style>
