29f720ccb6d79060ceffdb3051e362d4739bcf7a
Yusuf <yusufonarandx@hotmail.com>
2025-05-24 20:58:31 +0300

    style: update dark mode text color in ThemeSelector component

--- components/ThemeSelector.vue
+++ components/ThemeSelector.vue
@@ -12,6 +12,6 @@ const toggleColorMode = () => {
 <template>
   <div @click="toggleColorMode" class="flex items-center gap-2 cursor-pointer select-none">
     <component :is="colorMode.value === 'light' ? SunIcon : MoonIcon"
-      class="w-5 h-5 text-gray-600 dark:text-gray-300 transition-colors duration-300" />
+      class="w-5 h-5 text-gray-600 dark:text-gray-700 transition-colors duration-300" />
   </div>
 </template>
