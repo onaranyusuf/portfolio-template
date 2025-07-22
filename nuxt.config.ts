74b7d1550a81c751260f1a5bb26be5680fa47044
Yusuf <yusufonarandx@hotmail.com>
2025-07-22 12:08:58 +0300

    feat: ssr true

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ import { defineNuxtConfig } from "nuxt/config";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  ssr: true,
   css: ["~/assets/css/main.css"],
   vite: {
     plugins: [tailwindcss()],
